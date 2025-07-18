3e0d5ae57f18c8b373d715f8a7a383d7fed62c58
ada <abhijitadadas@gmail.com>
2025-07-19 03:01:36 +0530

    commit

--- proverbs.go
+++ proverbs.go
@@ -1,1 +1,8 @@
 package main
+
+// Dont communicate by sharing memory , share memory by communicating.
+// Channels are better than mutex in terms of usage.
+// A little copying is better than a little dependencies 
+// Clear is better than clever 
+// Errors are just values
+// Documentation is used for users.
